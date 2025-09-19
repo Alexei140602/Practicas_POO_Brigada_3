20e4bf3367bdb469dc741aadf6ef620c894d119d
Alexei140602 <133258992+Alexei140602@users.noreply.github.com>
2025-09-19 08:29:44 -0600

    Add files via upload

---
+++ Proyecto_1/Carrito.java
@@ -0,0 +1,10 @@
+public class Carrito {
+    String articulo;
+    double precio;
+    public Carrito(){
+    }
+    public Carrito (String articulo, double precio){
+        this.articulo=articulo;
+        this.precio=precio;
+    }
+}
