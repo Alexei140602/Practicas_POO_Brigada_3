0698bf8ec93d70ad8d254922d336dbbf7ab2252f
Alexei140602 <133258992+Alexei140602@users.noreply.github.com>
2025-09-19 08:28:11 -0600

    Add files via upload

---
+++ Examen_Practico/Compras.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Compras 
+{
+     ArrayList <Carrito> listaCarritos = new ArrayList<Carrito>();
+    public void Agregar(String articulo, double precio)
+    {
+       
+    }
+    public void Eliminar(int numeroEliminar)
+    {
+       
+    }
+    public void Mostrar()
+    {
+
+    }    
+}
