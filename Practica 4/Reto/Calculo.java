d0f9400dc977dba37c516c4f922d2ed067714952
Edgar7723 <martined2315@gmail.com>
2025-09-23 14:26:51 -0600

    Calculo.java

--- Practica 4/Reto/Calculo.java
+++ Practica 4/Reto/Calculo.java
@@ -1,43 +1,44 @@
-import  java.lang.Math;
+import java.lang.Math;
+
 public class Calculo
 {
     double a,b,c;
+
     public String distancia(Punto a, Punto b, Punto c) 
-{
+    {
         double distanciaAB, distanciaAC, distanciaBC;   
         double d1,d2,d3;
         double diffXAB = b.x - a.x;
-        double diffYAB= b.y - a.y;
+        double diffYAB = b.y - a.y;
         double diffXAC = c.x - a.x;
-        double diffYAC= c.y - a.y;
+        double diffYAC = c.y - a.y;
         double diffXBC = c.x - b.x;
-        double diffYBC= c.y - b.y;
-        distanciaAB=Math.sqrt(Math.pow((diffXAB), 2) + Math.pow(diffYAB, 2));
-        distanciaAC=Math.sqrt(Math.pow((diffXAC), 2) + Math.pow(diffYAC, 2));
-        distanciaBC=Math.sqrt(Math.pow((diffXBC), 2) + Math.pow(diffYBC, 2));
-        d1=Math.abs(distanciaAB-distanciaAC);
-        d2=Math.abs(distanciaAB-distanciaBC);
-        d3=Math.abs(distanciaAC-distanciaBC);
+        double diffYBC = c.y - b.y;
 
-        if(distanciaAB==distanciaAC && distanciaAB==distanciaBC && distanciaAC==distanciaBC)
+        
+        distanciaAB = Math.round(Math.sqrt(Math.pow(diffXAB, 2) + Math.pow(diffYAB, 2)) * 10) / 10.0;
+        distanciaAC = Math.round(Math.sqrt(Math.pow(diffXAC, 2) + Math.pow(diffYAC, 2)) * 10) / 10.0;
+        distanciaBC = Math.round(Math.sqrt(Math.pow(diffXBC, 2) + Math.pow(diffYBC, 2)) * 10) / 10.0;
+
+        d1 = Math.abs(distanciaAB - distanciaAC);
+        d2 = Math.abs(distanciaAB - distanciaBC);
+        d3 = Math.abs(distanciaAC - distanciaBC);
+
+        
+        if ((a.x==b.x && a.y==b.y) || (a.x==c.x && a.y==c.y) || (b.x==c.x && b.y==c.y))
         {
-            return "ingresaste el mismo punto";
+            return "Ingresaste el mismo punto";
         }
         else
         {
-            if (d1<0.01 && d2<0.01 && d3<0.01 )
-        {
-                return " Los puntos forman un triangulo equilatero y la distancia entre los puntos es:" +" " +distanciaAB;
-        }
-    
+            if (d1 < 0.1 && d2 < 0.1 && d3 < 0.1)
+            {
+                return "Los puntos forman un triangulo equilatero y la distancia entre los puntos es: " + distanciaAB;
+            }
             else
             {
-                return "los puntos no forman un triangulo equilatero";
+                return "Los puntos no forman un triangulo equilatero";
             }
         }
-
-        
-    
-}
-
+    }
 }
