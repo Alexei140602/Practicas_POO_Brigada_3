563a67f24e861e87b3d2c9158408e863ec6b18bc
Edgar7723 <martined2315@gmail.com>
2025-12-01 23:15:15 -0600

    Update AudioManager.dart

--- Proyecto_3/POO_proyecto3/lib/source/AudioManager.dart
+++ Proyecto_3/POO_proyecto3/lib/source/AudioManager.dart
@@ -8,7 +8,7 @@ class AudioManager {
   final AudioPlayer _audioPlayer = AudioPlayer();
   bool _isPlaying = false;
 
-  // Cargar y reproducir música de fondo
+
   Future<void> playBackgroundMusic(String assetPath) async {
     try {
       await _audioPlayer.stop();
@@ -21,7 +21,7 @@ class AudioManager {
     }
   }
 
-  // Reproducir efecto de sonido (sin loop)
+ 
   Future<void> playSoundEffect(String assetPath) async {
     try {
       final audioPlayer = AudioPlayer();
@@ -32,28 +32,28 @@ class AudioManager {
     }
   }
 
-  // Pausar música
   Future<void> pauseMusic() async {
     await _audioPlayer.pause();
     _isPlaying = false;
   }
 
-  // Reanudar música
+  
   Future<void> resumeMusic() async {
     await _audioPlayer.resume();
     _isPlaying = true;
   }
 
-  // Detener música
+ 
   Future<void> stopMusic() async {
     await _audioPlayer.stop();
     _isPlaying = false;
   }
 
-  // Control de volumen (0.0 a 1.0)
+ 
   Future<void> setVolume(double volume) async {
     await _audioPlayer.setVolume(volume);
   }
 
   bool get isPlaying => _isPlaying;
 }
+
